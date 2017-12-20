b45656a51ff6cc407d64cd193b5bd1dfc2768336
Mahdi Ebrahimi <azkia98@github.com>
2017-12-20 11:05:48 +0330

    [bug]: fix the splash win bug

--- main.js
+++ main.js
@@ -10,14 +10,6 @@ app.on('ready', () => {
 
     });
 
-    let splashWin=new BrowserWindow({
-        width:500,
-        height:400,
-        frame:false
-    });
-
-
-
     let win = new BrowserWindow({
         width: mainWindowState.width,
         height: mainWindowState.height,
@@ -26,6 +18,16 @@ app.on('ready', () => {
         show:false
     });
 
+    let splashWin=new BrowserWindow({
+        width:500,
+        height:400,
+        frame:false
+    });
+
+
+
+    
+
 
     win.on('closed',()=>{
         app.quit();
@@ -33,14 +35,15 @@ app.on('ready', () => {
     });
 
     splashWin.on('closed',()=>splashWin=null);
-
     win.on('ready-to-show',()=>{
+        console.log('2')
         win.show();
-        splashWin.close();
+        splashWin.destroy();
     });
 
-    mainWindowState.manage(win);
+    
     win.loadURL(`file://${__dirname}/index.html`);
     splashWin.loadURL(`file://${__dirname}/hello.html`);
+    mainWindowState.manage(win);
 });
 
